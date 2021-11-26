6a99aaa9dda3e76e82c45d205e51f505547553d9
ale88sera <ale88sera@gmail.com>
2021-11-26 17:04:14 +0100

    set up custom background image

--- header.php
+++ header.php
@@ -16,11 +16,16 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<style>
+		body.sub-custom-bg {
+			background-image: url("<?php the_post_thumbnail_url() ?>");
+		}
+	</style>
 
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class("sub-custom-bg"); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'subliquidawp' ); ?></a>
