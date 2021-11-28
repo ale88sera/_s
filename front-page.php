21b0f776ae206c76126b06d79a196820957ca76a
ale88sera <ale88sera@gmail.com>
2021-11-28 11:51:07 +0100

    create front-page files

---
+++ front-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the front-page
+ *
+ * This is the template that displays the front-page by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package subliquidawp
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'frontpage' );
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
